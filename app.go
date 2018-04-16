ea2d910769f9cffd69c5e129430374915254b4bc
Zach Gilbert <zsgilber@gmail.com>
2018-04-15 23:06:23 -0700

    make it better

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -32,7 +33,7 @@ func (a *App) Initialize(user, password, dbname string) {
 }
 
 func (a *App) Run(addr string) {
-	log.Fatal(http.ListenAndServe(":8000", a.Router))
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), a.Router))
 }
 
 func (a *App) getLead(w http.ResponseWriter, r *http.Request) {
